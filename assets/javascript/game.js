7a9fd11e23daf0f4657aab55bd6ee9e39146d0dc
hoang <hoangcao@mail.weber.edu>
2018-08-16 13:43:28 -0600

    random character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,19 @@
 
 
-document.onload = function(){
+ var img = ["db1.jpg", "db2.jpg", "db3.jpg", "db4.jpg"]
+ function imgRandom(imgArr) {
+    return imgArr[Math.floor(Math.random() * imgArr.length)];
+}
+    imgRandom(img)
 
-document.querySelector("row").style.visibility = "hidden";   
- 
- $(".play").on("click", function() 
+ function play() 
  {
-    alert("aaaaa");
- });
-
-document.querySelector("div#layer1").style.visibility = 'hidden';    
-document.querySelector("div#layer1").style.visibility = 'visible';
-
+    alert("Hello! I am an alert box!!");
+    document.querySelector(".hidden").style.display = "initial";
+    document.querySelector(".hidden1").style.display = "initial";
+    document.querySelector(".hidden3").innerHTML = "<h1>You will get a random character</h1>"
+    document.querySelector("#footer").style.display = "initial";
+    document.querySelector(".img-thumbnail").src="./assets/images/"+imgRandom(img);
  }
+
+ 
