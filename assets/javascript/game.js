65c43677deaaeccd559856b6b8fc96e7672e3c9a
hoang <hoangcao@mail.weber.edu>
2018-08-16 12:05:27 -0600

    add pictures

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+document.onload = function(){
+
+document.querySelector("row").style.visibility = "hidden";   
+ 
+ $(".play").on("click", function() 
+ {
+    alert("aaaaa");
+ });
+
+document.querySelector("div#layer1").style.visibility = 'hidden';    
+document.querySelector("div#layer1").style.visibility = 'visible';
+
+ }
